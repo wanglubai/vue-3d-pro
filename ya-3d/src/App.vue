140b903cd1bc944a6a378a610f464daed694510d
王爽 <wangshuang@plu.cn>
2018-06-07 17:30:16 +0800

    init

---
+++ ya-3d/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="ya-container">
+<el-button type="primary" icon="el-icon-edit">asasasas</el-button>
+</div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+.ya-container {
+  background-color: black;
+  width: 1400px;
+  height: 800px;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
